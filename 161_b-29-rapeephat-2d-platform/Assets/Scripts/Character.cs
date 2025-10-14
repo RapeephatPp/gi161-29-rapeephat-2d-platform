998b547d93fdca52b1aa77f1c8ca734f0b01cb53
RapeephatPp <rapeephat.prat@bumail.net>
2025-10-14 16:52:09 +0700

    Base Script Added

---
+++ 161_b-29-rapeephat-2d-platform/Assets/Scripts/Character.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    private int health;
+
+    public int Health
+    {
+        get { return health; }
+        set { health = (value < 0) ? 0 : value; }
+    }
+    
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Debug.Log($"{this.name} took damage {damage}! Current health is {Health}");
+
+        IsDead();
+    }
+
+    public bool IsDead()
+    {
+        if( Health <= 0 )
+            {
+            Destroy(this.gameObject);
+            return true;
+            }
+        else { return false; }
+    }   
+    
+    void Start()
+    {
+        
+    }
+}
